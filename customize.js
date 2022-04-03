453eef04ff8582946d63aac41ae91c5662ee88b1
Brian Chen <brianchen831@gmail.com>
2022-04-02 21:27:56 -0500

    commit before i start to work on save changes sob

---
+++ customize.js
@@ -0,0 +1,35 @@
+//add customizations
+function s2c1(){
+    document.getElementById('sec2').src = 'hangmanSection2Custom1.png';
+}
+function s2c2(){
+    document.getElementById('sec2').src = 'hangmanSection2Custom2.png';
+}
+
+
+function s3c1(){
+    document.getElementById('sec3').src = 'hangmanSection3Custom1.png';
+}
+function s3c2(){
+    document.getElementById('sec3').src = 'hangmanSection3Custom2.png';
+}
+
+
+function s4c1(){
+    document.getElementById('sec4').src = 'hangmanSection4Custom1.png';
+}
+function s4c2(){
+    document.getElementById('sec4').src = 'hangmanSection4Custom2.png';
+}
+
+
+function reset(){
+    document.getElementById('sec2').src = 'hangmanSection2.png';
+    document.getElementById('sec3').src = 'hangmanSection3.png';
+    document.getElementById('sec4').src = 'hangmanSection4.png';
+}
+
+//store info to be used in hangman
+function save(){
+    
+}
